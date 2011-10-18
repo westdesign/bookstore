fa582a11a416bfa783085325a0d207152bf6cf4f
Thomas Fankhauser <tommylefunk@googlemail.com>
2011-10-18 16:29:01 +0200

    Added some sample books to close #3

--- bookstore.rb
+++ bookstore.rb
@@ -3,7 +3,18 @@ module Bookstore
   
   # Prints all books available
   def self.list
-    # stub
+    
+    # create sample library
+    sample_library = [
+      Book.new(:title => "Sample Title 1", :author => "Sample Author 1", :year => 2011),
+      Book.new(:title => "Sample Title 2", :author => "Sample Author 2", :year => 2011),
+      Book.new(:title => "Sample Title 3", :author => "Sample Author 3", :year => 2011),
+      Book.new(:title => "Sample Title 4", :author => "Sample Author 4", :year => 2011),
+    ]
+    
+    # print them
+    sample_library.each { |book| puts book }
+      
   end
   
 end
--- lib/book.rb
+++ lib/book.rb
@@ -4,6 +4,13 @@ module Bookstore
     attr_accessor :author
     attr_accessor :year
     
+    # Initialize with attributes
+    def initialize attributes
+      @title   = attributes[:title]
+      @author  = attributes[:author]
+      @year    = attributes[:year]
+    end
+    
     # Pretty print a book
     def to_s
       "#{title} by #{author} written in #{year}"
