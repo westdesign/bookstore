bd7db2fca0528f9955b2437cfb7190ad340093e3
Thomas Fankhauser <tommylefunk@googlemail.com>
2011-10-18 16:49:14 +0200

    Added some more books

--- lib/library.rb
+++ lib/library.rb
@@ -5,9 +5,12 @@ module Bookstore::Library
   # Returns all books in the library
   def self.books
     @@books = [
-      Bookstore::Book.new(:title => "iOS Sensors",           :author => "Tim Cook",        :year => 2011),
-      Bookstore::Book.new(:title => "Android for Designers", :author => "Ursula N. Known", :year => 2011),
-      Bookstore::Book.new(:title => "Pro Git",               :author => "Scott Chacon",    :year => 2010)
+      Bookstore::Book.new(:title => "iOS Sensors",           :author => "Tim Cook",         :year => 2011),
+      Bookstore::Book.new(:title => "Android for Designers", :author => "Ursula N. Known",  :year => 2011),
+      Bookstore::Book.new(:title => "Pro Git",               :author => "Scott Chacon",     :year => 2010),
+      Bookstore::Book.new(:title => "Gitorious",             :author => "Works N. Well",    :year => 2009),
+      Bookstore::Book.new(:title => "Ruby on Rails",         :author => "David H. Hansson", :year => 2011),
+      Bookstore::Book.new(:title => "Redis Pro",             :author => "Roland Edis",      :year => 2012)
     ]
   end
   
